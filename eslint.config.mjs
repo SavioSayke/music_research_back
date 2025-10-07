1498a0306aa60ad433fbb2512f14468d8ca6645c
ThulioBezerra <thulioalves20@gmail.com>
2025-10-07 20:41:32 -0300

    chore: add config to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,9 +26,22 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error', 
+      'no-console': 'warn',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      "prettier/prettier": [
+        "error",
+        {
+          "endOfLine": "auto"
+        }
+      ]
     },
-  },
+      
+    },
+  
 );
